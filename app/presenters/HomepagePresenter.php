ffa7ee698949fb9150097bb46fe1270e8a60f456
Jaroslav Malý <jaroslav.maly@zasilkovna.cz>
2019-01-02 14:27:20 +0100

    remove notorm

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,14 +4,10 @@ namespace App\Presenters;
 
 use Nette;
 
-
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-	/** @var \App\Models\UserModel @inject */
-	public $userModel;
 
 	public function renderDefault() {
-		$this->template->data = $data = $this->userModel->getByName('jarda');
-		bdump($data['surname']);
+
 	}
 }
